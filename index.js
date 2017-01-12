0dbe7d67c2073f8a96edaa3e24039effb2c39c8b
shubham raj <shubhamraj97@gmail.com>
2017-01-12 15:57:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ var oxford = require('project-oxford'),
     client = new oxford.Client('631297ea8915437f80129990b2b491e8');
 
 var NodeWebcam = require( "node-webcam" );
-
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'student-shubhamraj', 
+  api_key: '665371917748924', 
+  api_secret: 'gjaPdGyD5UJvppfSl3l_wjcbuC0' 
+});
 
 //Default options
 
@@ -42,16 +47,19 @@ var name=socket.id;console.log(name);
 
 //Will automatically append location output type
  socket.on('click',function(data){
-   var locat1='/public/'+name;
-   Webcam.capture( locat1,function(err,res){
+   var locat1='C:/Users/User/Desktop/face-detection/face-detection-master/public/'+name;
+   Webcam.capture(function(err){
      console.log('soka');
+	 cloudinary.uploader.upload("test_picture.jpeg", function(result) { 
+  console.log(result) 
+});
    } );
    console.log("working");
  });
  socket.on('result',function(data){
-   console.log("working2");var located='/public/'+name+'.jpg';console.log(located);
+   console.log("working2");var located='C:/Users/User/Desktop/face-detection/face-detection-master/public/'+name+'.jpg';console.log(located);
    client.face.detect({
-    path: located,
+    url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSjTKxD56iMMj5KD5q4n0tRUBRyTqbOgIIz518L2jyKPxlsKQaFFg',
     analyzesAge: true,
     analyzesGender: true
 }).then(function (response) {
